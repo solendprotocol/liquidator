4ebfdbbfabed725dc7ddd66b754002670ddc2963
0xCactus <85321734+0xCactus@users.noreply.github.com>
2022-07-28 09:59:26 -0700

    specify path in readSecret console error

--- src/libs/secret.ts
+++ src/libs/secret.ts
@@ -7,7 +7,7 @@ export function readSecret(secretName) {
 	} catch (err) {
 		if (err.code !== 'ENOENT') {
 			console.error(
-				`An error occurred while trying to read the secret: ${secretName}. Err: ${err}`
+				`An error occurred while trying to read the secret path: ${path}. Err: ${err}`
 			)
 		} else {
 			console.debug(`Could not find the secret,: ${secretName}. Err: ${err}`)
