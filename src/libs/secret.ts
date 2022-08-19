a629aafdc0988e62bafd45ff4a100ce186840c8e
legocactus <0xcactus@solend.fi>
2022-08-19 16:24:36 -0400

    fix secrets.ts

--- src/libs/secret.ts
+++ src/libs/secret.ts
@@ -1,17 +1,17 @@
-import fs from 'fs'
+import fs from 'fs';
 
 export function readSecret(secretName) {
-	try {
-		const path = process.env.SECRET_PATH || `/run/secrets/${secretName}`
-		return fs.readFileSync(path, 'utf8')
-	} catch (err) {
-		if (err.code !== 'ENOENT') {
-			console.error(
-				`An error occurred while trying to read the secret path: ${path}. Err: ${err}`
-			)
-		} else {
-			console.debug(`Could not find the secret,: ${secretName}. Err: ${err}`)
-		}
-		return ''
-	}
+  const path = process.env.SECRET_PATH || `/run/secrets/${secretName}`;
+  try {
+    return fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      console.error(
+        `An error occurred while trying to read the secret path: ${path}. Err: ${err}`,
+      );
+    } else {
+      console.debug(`Could not find the secret,: ${secretName}. Err: ${err}`);
+    }
+    return '';
+  }
 }
