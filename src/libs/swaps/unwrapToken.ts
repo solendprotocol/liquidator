6de4eef69f2984c8a9623658eb09251361c3a98f
legocactus <0xcactus@solend.fi>
2022-09-21 16:07:11 +0800

    fix layout buffer error

--- src/libs/swaps/unwrapToken.ts
+++ src/libs/swaps/unwrapToken.ts
@@ -1,52 +1,47 @@
-import { Account, Connection, Keypair, PublicKey } from "@solana/web3.js";
-import { unwrapNazareLp, getNazareTokenMints } from "./nazare-nlp/unwrapNazareLp";
-import { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID } from "@solana/spl-token";
+/* eslint-disable no-restricted-syntax */
+import { Account, Connection, PublicKey } from '@solana/web3.js';
+import { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { unwrapNazareLp, getNazareTokenMints } from './nazare-nlp/unwrapNazareLp';
 
 export const findAssociatedTokenAddress = async (
   walletAddress: PublicKey,
-  tokenMintAddress: PublicKey
-) => {
-  return (
-    await PublicKey.findProgramAddress(
-      [walletAddress.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), tokenMintAddress.toBuffer()],
-      ASSOCIATED_TOKEN_PROGRAM_ID
-    )
-  )[0]
-}
+  tokenMintAddress: PublicKey,
+) => (
+  await PublicKey.findProgramAddress(
+    [walletAddress.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), tokenMintAddress.toBuffer()],
+    ASSOCIATED_TOKEN_PROGRAM_ID,
+  )
+)[0];
 
 export const getWalletBalance = async (
   connection: Connection,
   mint: PublicKey,
-  walletAddress: PublicKey
+  walletAddress: PublicKey,
 ): Promise<number> => {
-  const userAta = await findAssociatedTokenAddress(walletAddress, mint)
+  const userAta = await findAssociatedTokenAddress(walletAddress, mint);
 
-  return await connection
+  return connection
     .getTokenAccountBalance(userAta)
     .then((tokenAmount) => {
       if (parseFloat(tokenAmount?.value?.amount)) {
-        return parseFloat(tokenAmount.value.amount)
-      } else {
-        return 0
+        return parseFloat(tokenAmount.value.amount);
       }
+      return 0;
     })
-    .catch((error) => {
-      return 0
-    })
-}
-
+    .catch((error) => 0);
+};
 
 export const unwrapToken = async (
   connection: Connection,
   payer: Account,
 ) => {
   // Nazare LP tokens 
-  const nazareMints = await getNazareTokenMints(connection)
+  const nazareMints = await getNazareTokenMints(connection);
   for (const mint of nazareMints) {
     // check if wallet has Nazare LP tokens
-    const tokenAmount = await getWalletBalance(connection, mint, payer.publicKey)
+    const tokenAmount = await getWalletBalance(connection, mint, payer.publicKey);
     if (tokenAmount) {
-      unwrapNazareLp(connection, payer, mint, tokenAmount)
+      await unwrapNazareLp(connection, payer, mint, tokenAmount);
     }
-  } 
-}
+  }
+};
